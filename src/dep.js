c96f4133b516017a53e5b875847172f6b9d17a8b
wangmin23 <wangmin23@meituan.com>
2018-09-02 16:34:15 +0800

    update: Dep 类发布订阅 用

---
+++ src/dep.js
@@ -0,0 +1,17 @@
+class Dep {
+  constructor () {
+    this.subs = []
+  }
+
+  addSub (watcher) {
+    this.subs.push(watcher)
+  }
+
+  notify () {
+    this.subs.forEach((watcher) => {
+      watcher.update()
+    })
+  }
+}
+
+console.log(Dep)
